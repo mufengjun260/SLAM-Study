1eee04059a310c4100b28347a40ffa00a72ac7c9
mufengjun260 <abcdeqq3.141592>
2019-10-17 15:15:30 +0800

    added demo of ORB algorithm

---
+++ ORB.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('/home/mufengjun260/PycharmProjects/SLAM-Study/Sample1.jpg', 0)
+
+orb = cv2.ORB_create()
+
+kp = orb.detect(img, None)  # 描述符
+
+img = cv2.drawKeypoints(img, kp, img, color=(255, 0, 0))  # 画到img上面
+
+plt.imshow(img)
+plt.show()
